1c5ef32e8c50943029bf9d9b2cd402c7911b39e8
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-09-22 22:55:42 -0300

    TIK change App

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
